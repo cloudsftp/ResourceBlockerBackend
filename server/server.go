5ebc9cc86819f118128af5b1bc8ce40560999e8e
Fabian Weik <fabian.weik@protonmail.com>
2022-07-30 11:58:55 +0200

    disable workflow for feature branch

--- server/server.go
+++ server/server.go
@@ -25,6 +25,7 @@ func newResourceResponse(config *Config) *resourcesResponse {
 	for id := range config.Resources {
 		ids = append(ids, id)
 	}
+	return &resourcesResponse{ResourceIds: ids}
 }
 
 type updateStatusRequest struct {
